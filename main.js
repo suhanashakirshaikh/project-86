fd70c7fc5adb52dd2a648457522aeeb30bb313db
suhanashakirshaikh <83486875+suhanashakirshaikh@users.noreply.github.com>
2021-10-12 13:18:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL('BirthdayImage.jpg', function(Img){
+        block_image_object = Img;
+
+        block_image_object
+    }
+	
+	
+}
+
+function playSound(){
+	
+}
